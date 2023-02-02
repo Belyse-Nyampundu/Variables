4353a0f17ce63e9a3f1c1ef4d24363c4369664c2
Belyse <belysenyampundu@gmail.com>
2023-02-02 08:27:06 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var fullname = "Nyampundu belyse"
+    var age = 44
+    var phonenumber = 250783239291
+    var weight = 60
+    var citizen = "Rwandan"
+            
+
+}
